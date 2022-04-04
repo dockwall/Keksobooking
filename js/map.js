380d5508f4b3a93e01e7294f9bd388224bdb831f
Stas <leshuksu@gmail.com>
2022-04-04 14:28:18 +0300

    Started similar ads, created mock arrays

---
+++ js/map.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const IMG_AVATARS = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11'];
+const TITLES = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
+const PLACE_TYPES = ['palace', 'flat', 'house', 'bungalo'];
+const TIMES = ['12:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
