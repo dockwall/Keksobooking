eabd91266c38fca1ba8e419d3c026fe3be50b778
Stas <leshuksu@gmail.com>
2022-05-21 00:31:46 +0300

    Refactored utils functions, moved them to data.js

--- js/data.js
+++ js/data.js
@@ -5,9 +5,37 @@
 
   const offersArray = [];
 
+  const getRandomFromInterval = (interval) => {
+    return Math.floor((interval.MAX - interval.MIN + 1) * Math.random()) + interval.MIN;
+  };
+
+  const getRandomFromArray = (array) => {
+    const randomIndex = Math.floor(array.length * Math.random());
+    return array[randomIndex];
+  };
+
+  const getShuffledArray = (array, isSliced = false) => {
+    const originalArray = array.slice();
+    const shuffledArray = [];
+    let arrayLength = originalArray.length;
+
+    if (isSliced) {
+      arrayLength = Math.floor(originalArray.length * Math.random() + 1);
+    }
+
+    for (let i = 0; i < arrayLength; i++) {
+      const randomIndex = Math.floor(originalArray.length * Math.random());
+      const arrayElement = originalArray[randomIndex];
+      originalArray.splice(randomIndex, 1);
+      shuffledArray.push(arrayElement);
+    }
+
+    return shuffledArray;
+  };
+
   const generateOfferObject = (i) => {
-    const randomLocationX = window.utils.getRandomFromInterval(window.constants.offerOptions.LOCATION_X);
-    const randomLocationY = window.utils.getRandomFromInterval(window.constants.offerOptions.LOCATION_Y);
+    const randomLocationX = getRandomFromInterval(window.constants.offerOptions.LOCATION_X);
+    const randomLocationY = getRandomFromInterval(window.constants.offerOptions.LOCATION_Y);
 
     const offerObject = {
       author: {
@@ -17,15 +45,15 @@
       offer: {
         title: window.constants.offerOptions.TITLES[i],
         address: `${randomLocationX}, ${randomLocationY}`,
-        price: window.utils.getRandomFromInterval(window.constants.offerOptions.PRICES),
-        type: window.utils.getRandomFromArray(window.constants.offerOptions.TYPES),
-        rooms: window.utils.getRandomFromInterval(window.constants.offerOptions.ROOMS),
-        guests: window.utils.getRandomFromInterval(window.constants.offerOptions.GUESTS),
-        checkin: window.utils.getRandomFromArray(window.constants.offerOptions.TIMES),
-        checkout: window.utils.getRandomFromArray(window.constants.offerOptions.TIMES),
-        features: window.utils.getRandomArray(window.constants.offerOptions.FEATURES),
+        price: getRandomFromInterval(window.constants.offerOptions.PRICES),
+        type: getRandomFromArray(window.constants.offerOptions.TYPES),
+        rooms: getRandomFromInterval(window.constants.offerOptions.ROOMS),
+        guests: getRandomFromInterval(window.constants.offerOptions.GUESTS),
+        checkin: getRandomFromArray(window.constants.offerOptions.TIMES),
+        checkout: getRandomFromArray(window.constants.offerOptions.TIMES),
+        features: getShuffledArray(window.constants.offerOptions.FEATURES, true),
         desrciption: '',
-        photos: window.utils.getShuffledArray(window.constants.offerOptions.PHOTOS),
+        photos: getShuffledArray(window.constants.offerOptions.PHOTOS),
         location: {
           x: randomLocationX,
           y: randomLocationY,
