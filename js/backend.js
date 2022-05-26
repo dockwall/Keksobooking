1f4bbc6f3481626fe8244f1677735f1615b9a8af
Stas <leshuksu@gmail.com>
2022-05-27 01:37:26 +0300

    Added function to submit form to server

--- js/backend.js
+++ js/backend.js
@@ -28,7 +28,35 @@
     xhr.send();
   };
 
+  const saveForm = (formData, onLoad, onError) => {
+    const URL = 'https://22.javascript.pages.academy/keksobooking';
+
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.timeout = 5000;
+
+    xhr.addEventListener('load', function (evt) {
+      if (evt.target.status === 200) {
+        onLoad(evt.target.response);
+      } else {
+        onError('Статус ответа:' + evt.target.status + '. ' + evt.target.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения. Проверьте подключение к интернету');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Превышено время ожидания ответа сервера');
+    });
+
+    xhr.open('POST', URL);
+    xhr.send(formData);
+  };
+
   window.backend = {
     loadData,
+    saveForm,
   };
 })();
