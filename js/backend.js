cd7feae8a9e56370673899efd2eca38cf7e16740
Stas <leshuksu@gmail.com>
2022-05-27 03:00:50 +0300

    Refactored backend.js + now error message can be closed

--- js/backend.js
+++ js/backend.js
@@ -1,27 +1,44 @@
 'use strict';
 
 (function () {
-  const loadData = (onLoad, onError) => {
-    const URL = 'https://22.javascript.pages.academy/keksobooking/data';
+  const ALL_URL = {
+    forLoad: 'https://22.javascript.pages.academy/keksobooking/data',
+    forSave: 'https://22.javascript.pages.academy/keksobooking',
+  };
 
+  const ERROR_MESSAGES = {
+    serverError: 'Произошла ошибка сервера, повторите попытку позднее',
+    connectionError: 'Произошла ошибка соединения. Проверьте подключение к интернету',
+    timeoutError: 'Превышено время ожидания ответа сервера. Проверьте подключение к интернету',
+
+  };
+
+  const createXhr = () => {
     const xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
     xhr.timeout = 5000;
+
+    return xhr;
+  };
+
+  const loadData = (onLoad, onError) => {
+    const URL = ALL_URL.forLoad;
+    const xhr = createXhr();
 
     xhr.addEventListener('load', function (evt) {
       if (evt.target.status === 200) {
         onLoad(evt.target.response);
       } else {
-        onError('Статус ответа:' + evt.target.status + '. ' + evt.target.statusText);
+        onError(`${ERROR_MESSAGES.serverError}.\n ${evt.target.status}: ${evt.target.statusText}`);
       }
     });
 
     xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения. Проверьте подключение к интернету');
+      onError(ERROR_MESSAGES.connectionError);
     });
 
     xhr.addEventListener('timeout', function () {
-      onError('Превышено время ожидания ответа сервера');
+      onError(ERROR_MESSAGES.timeoutError);
     });
 
     xhr.open('GET', URL);
@@ -29,11 +46,8 @@
   };
 
   const saveForm = (formData, onLoad, onError) => {
-    const URL = 'https://22.javascript.pages.academy/keksobooking';
-
-    const xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-    xhr.timeout = 5000;
+    const URL = ALL_URL.forSave;
+    const xhr = createXhr();
 
     xhr.addEventListener('load', function (evt) {
       if (evt.target.status === 200) {
@@ -44,11 +58,11 @@
     });
 
     xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения. Проверьте подключение к интернету');
+      onError(ERROR_MESSAGES.connectionError);
     });
 
     xhr.addEventListener('timeout', function () {
-      onError('Превышено время ожидания ответа сервера');
+      onError(ERROR_MESSAGES.timeoutError);
     });
 
     xhr.open('POST', URL);
--- js/error.js
+++ js/error.js
@@ -4,6 +4,7 @@
   const createErrorMessage = (errorText) => {
     const errorElement = document.createElement('div');
 
+    errorElement.id = 'error-message';
     errorElement.textContent = errorText;
 
     errorElement.style.position = 'fixed';
@@ -28,11 +29,24 @@
   const showErrorMessage = (errorText) => {
     const errorElement = createErrorMessage(errorText);
 
+    document.addEventListener('keydown', closeErrorMessage);
+    document.addEventListener('click', closeErrorMessage);
+
     window.form.successSaveMessage.insertAdjacentElement('beforebegin', errorElement);
 
     setTimeout(() => {
       errorElement.remove();
     }, 5000);
+  };
+
+  const closeErrorMessage = (evt) => {
+    if (evt.type === 'keydown' && evt.keyCode !== window.constants.keyCodes.ESC) {
+      return;
+    }
+
+    document.querySelector('#error-message').remove();
+    document.removeEventListener('keydown', closeErrorMessage);
+    document.removeEventListener('click', closeErrorMessage);
   };
 
   window.error = {
